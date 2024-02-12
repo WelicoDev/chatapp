3a4aaf1ac00cc4e10ea665b6def2624c6a9b77a9
welicodev <welicodev@kali.www>
2024-02-11 22:59:22 -0500

    new project

---
+++ room/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    room = models.ForeignKey(Room , on_delete=models.CASCADE , related_name="messages")
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    content = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('date_added',)
---
+++ room/routing.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .consumers import ChatConsumer
+
+
+
+websocket_urlpatterns = [
+    path('ws/<str:room_name>/' , ChatConsumer.as_asgi()),
+]
+
